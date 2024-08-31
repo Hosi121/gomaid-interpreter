2528a7bb586be677fcca5a0d58b8892222979baa
Hosi121 <takuyakataiwa@icloud.com>
2024-09-01 04:55:23 +0900

    sec1.3: Lexerを最低限定義

---
+++ lexer/lexer.go
@@ -0,0 +1,23 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // 現在の文字の位置
+	readPosition int  // これから読み込む位置
+	ch           byte // 現在検査中の文字
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
